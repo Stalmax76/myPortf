9b335a37f8c1ca97069461e1614f2180f2307e6a
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-04-09 14:57:21 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
+
+
